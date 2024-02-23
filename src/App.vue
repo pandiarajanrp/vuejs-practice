0c02598b69ea9efc14ff4706f7a16fedaf496e6d
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-02-22 18:35:18 -0600

    rendering
    rendering

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <h2>{{ name }}</h2>
+    <h1>Variable Binding</h1>
+    <h4>{{ name }}</h4>
+    <h1>V-Bind</h1>
     <div class="underline" v-bind:class="status">This is text</div>
     <div v-bind:id="containerId">This is content container</div>
     <p v-bind:class="isPromoted && 'promoted'">This is promoted content</p>
@@ -18,17 +20,35 @@
     >
       Object conditional class binding
     </p>
+    <h1>V-Bind Shorthand</h1>
     <p :class="isShort ? 'short' : 'non-short'">V bind Shorthand</p>
+
     <h1>Conditional Rendering</h1>
     <h4 v-if="num===0">Num is Zero</h4>
     <h4 v-else-if="num > 0">Positive</h4>
     <h4 v-else-if="num < 0">Negative</h4>
     <h4 v-else>Not a Number</h4>
+
     <h1>Conditional Rendering With Multiple Element</h1>
     <template v-if="showConditions">
       <p>Terms and conditions</p>
       <p>Privacy Policy</p>
       <p>Agree to Policy</p>
+    </template>
+
+    <h1>CR - V-Show</h1>
+    <p v-show="showUserName">This is username with display none prop on false</p>
+    <p v-if="showUserName">This is username with only render in dom if true</p>
+
+    <h1>List - V-For</h1>
+    <h4 v-for="(userName, index) in names" :key="userName">{{ index }} {{ userName }}</h4>
+
+    <div v-for="user in users" :key="user.name">
+      <h2>{{ user.name }}</h2>
+      <div v-for="tech in user.technology" :key="tech">{{ tech }}</div>
+    </div>
+    <template v-for="(info, key, index) in userInfo" :key="info">
+      <p>{{ info }} {{ key }} {{ index }}</p>
     </template>
   </div>
 </template>
@@ -46,7 +66,28 @@ export default {
       isNew: true,
       isShort: true,
       num: 5,
-      showConditions: true
+      showConditions: true,
+      showUserName: true,
+      names: ['Pandi', 'Selvam', 'Mohammed'],
+      users: [
+        {
+          name: "Pandi",
+          technology: ["html", "nodejs", "react", "vue"]
+        },
+        {
+          name: "Selvam",
+          technology: ["html", "nodejs", "react"]
+        },
+        {
+          name: "Mohammed",
+          technology: ["html", "nodejs", "react"]
+        }
+      ],
+      userInfo: {
+        fname: "Pandiarajan",
+        lname: "Rajagopal",
+        id: '29'
+      }
     };
   },
 };

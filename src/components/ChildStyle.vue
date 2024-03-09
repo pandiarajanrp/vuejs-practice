9e7f6137d5886c6a72d7d679e1d0956c89182dbf
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-03-08 18:33:52 -0600

    Create child style
    Create child style

---
+++ src/components/ChildStyle.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h4>This is from Child Style</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    h4 {
+        color: red;
+    }
+</style>
